e3267ebcc121d461df2cf52e6ce0458b6c28f128
Elliot Smith <elliot.smith@intel.com>
2014-01-31 16:02:51 +0000

    Add marconi to demos

---
+++ es-media-player/README.md
@@ -0,0 +1,43 @@
+Radio show player
+
+Features:
+
+*   "kindle for radio shows" (could also work for podcasts)
+*   plays mp3 files and extracts their id3 data
+*   series view is default and primary view (corresponding to a series of episodes for a single show, and represented in the mp3 metadata using the album tag)
+*   organise tracks within a series by track number
+*   show the last track you listened to prominently on the front panel, along with the next suggested track (defaults to the next episode in the series)
+*   show progress through each track, so you can see what you've already listened to and easily go back to an episode (both in the full list view and the series view)
+*   enable deletion of files from inside the player interface (fxos music doesn't do this)
+*   show full title in list of episodes and in the "now playing" screen (fxos music truncates everything)
+
+Later:
+
+*   make it easy to go to a series from an individual episode, so you can see what remains or easily go to a particular episode from the series you're listening to
+*   control from the lock screen
+*   list by artist (i.e. the producer of the radio broadcast)
+*   support for downloading files from a upnp server (when fxos supports this)
+*   support playlists (low priority, as a series is implicitly a tracklist)
+*   support for file types other than mp3 (low priority)
+
+NEVER:
+
+*   scrobbling
+*   shuffle
+*   album art
+*   volume control (the hardware does that, unless your phone is really crap)
+
+Design philosophy:
+
+*   don't try to be a music or video player: remember this is for playing radio shows and stay focused on that
+*   make it easy to control while walking around
+    *   BIG buttons
+    *   BIG sliders for scrubbing through a track, with clear indication of position in the track during scrub
+    *   BIG previous/next buttons
+    *   BIG rewind/ffwd buttons which skip a configurable number of seconds (starts with 30 seconds); makes it easy to skip over adverts at the start of a show
+    *   use the biggest font possible while still fitting 10 tracks on the screen (the upper limit for the number of episodes in a series)
+
+
+CREDITS
+
+radio icon from http://findicons.com/icon/457729/radio?id=457887 by wpzoom.com (CC-BY-SA)
---
+++ es-media-player/test/audio-fs-stub.js
@@ -0,0 +1,109 @@
+define(['lodash', 'jquery', 'stapes'], function (_, $, Stapes) {
+  var oggBase = require.toUrl('../../test/fixtures');
+
+  var files = [
+    {
+      filename: 'c',
+      url: oggBase + '/reading.ogg',
+      meta: {
+        album: 'Wagner tracks',
+        title: 'Wagner 3',
+        artist: 'Wagner',
+        tracknum: 3
+      }
+    },
+
+    {
+      filename: 'b',
+      url: oggBase + '/wagner-short.ogg',
+      meta: {
+        album: 'Wagner tracks',
+        title: 'Wagner 2',
+        artist: 'Wagner',
+        tracknum: 2
+      }
+    },
+
+    {
+      filename: 'a',
+      url: oggBase + '/reading.ogg',
+      meta: {
+        album: 'Wagner tracks',
+        title: 'Wagner 1',
+        artist: 'Wagner',
+        tracknum: 1
+      }
+    },
+
+    {
+      filename: 'h',
+      url: oggBase + '/reading.ogg',
+      meta: {
+        title: 'I Want My Mixer',
+        artist: 'Slobbo the Blob'
+      }
+    },
+
+    {
+      filename: 'e',
+      url: oggBase + '/wagner-short.ogg',
+      meta: {
+        album: 'Mighty War Music',
+        title: 'Holy Moses!',
+        artist: 'Frankie Moonlight',
+        tracknum: 2
+      }
+    },
+
+    {
+      filename: 'd',
+      url: oggBase + '/wagner-short.ogg',
+      meta: {
+        album: 'Mighty War Music',
+        title: 'Briefs Encounter',
+        artist: 'Charley Plop',
+        tracknum: 1
+      }
+    },
+
+    {
+      filename: 'f',
+      url: oggBase + '/reading.ogg',
+      meta: {
+        album: 'An Elaborate Evening\'s Entertainment with the Jocular Fellows',
+        title: 'I Have a Little Plop Tree',
+        artist: 'Charley Plop',
+        tracknum: 1
+      }
+    },
+
+    {
+      filename: 'g',
+      url: oggBase + '/wagner-short.ogg',
+      meta: {
+        title: 'I\'m Forever Wondering Where I Left My Hat, Presumably At Home',
+        artist: 'Frankie Moonlight'
+      }
+    }
+  ];
+
+  var obj = {
+    getAllFiles: function () {
+      var dfd = $.Deferred();
+      dfd.resolve(files);
+      return dfd.promise();
+    },
+
+    getFile: function (filename) {
+      var dfd = $.Deferred();
+
+      dfd.resolve(_.select(files, function (file) {
+        return file.filename === filename;
+      })[0]);
+
+      return dfd.promise();
+    }
+  };
+
+  return obj;
+});
